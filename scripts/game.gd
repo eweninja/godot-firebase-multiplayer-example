c7e52a0945fd08e38abf03987ca42a2f375e7de0
eweninja <eweninjadev@gmail.com>
2023-07-01 09:26:37 +0200

    Nicknames added.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 var player_scene = preload("res://scenes/player.tscn")
+var _nickname = ""
 
 @onready var firebase_manager = $FirebaseManager
 @onready var gameplay_node = $Gameplay
 
 @onready var login_node = $CanvasLayer/UI/MarginContainer/Login
+@onready var nickname_textedit : TextEdit = $CanvasLayer/UI/MarginContainer/Login/NicknameTextEdit
 @onready var login_button : Button = $CanvasLayer/UI/MarginContainer/Login/Login
 @onready var info_label = $CanvasLayer/UI/MarginContainer/Login/Info
 
@@ -13,6 +15,7 @@ signal login_failed(code, message)
 signal login_successed
 
 func _ready() -> void:
+	nickname_textedit.text = generate_random_nick()
 	
 	# Game signals
 	self.connect("login_failed", _on_login_failed)
@@ -23,10 +26,16 @@ func _ready() -> void:
 	
 	
 func _on_login_button_pressed():
+	var nickname = nickname_textedit.text
+	if nickname.length() < 3:
+		info_label.text = "Min. nickname length is 3."
+		return
+	
 	if !firebase_manager.is_logged_in():
 		info_label.text = "Loggining in..."
 		login_button.disabled = true
 		firebase_manager.login_anonymously()
+		_nickname = nickname
 	else:
 		pass
 		
@@ -38,20 +47,23 @@ func _on_login_failed(code, message):
 	
 func _on_login_successed():
 	login_node.hide()
-	_spawn_player()
+	_spawn_player(true, _nickname)
+	
+	
+func generate_random_nick() -> String:
+	var id = randi_range(1000, 9999)
+	return "Player#"+str(id)
 	
 	
 	
 	
 	
-	
-	
-	
-func _spawn_player(is_self = true):
+func _spawn_player(is_self = true, nickname = "placeholder"):
 	var x = randf_range(-400, 400)
 	var y = randf_range(-100, 100)
 	var player = player_scene.instantiate()
 	gameplay_node.add_child(player)
 	player.position = Vector2(x, y)
 	player.set_is_self(is_self)
+	player.set_nickname(nickname)
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 var _is_self = false
-
+var _nickname = ""
 const SPEED = 300.0
+
+@onready var nickname_label : Label = $Nickname
 
 func _physics_process(delta):
 	if !is_self(): return
@@ -25,3 +27,7 @@ func is_self() -> bool:
 
 func set_is_self(is_self_player):
 	_is_self = is_self_player
+
+func set_nickname(nick):
+	_nickname = nick
+	nickname_label.text = _nickname
